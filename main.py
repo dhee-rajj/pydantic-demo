9decb25f6cd5745a1d6b1ab5b3949ca39f783d0a
Code@2002 <venkatdheeraj.ind@gmail.com>
2024-09-09 13:43:23 +0530

    adding serizlizers

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 import json
 import argparse
 import dearpygui.dearpygui as dpg
+import re
 
 # Enum for account types
 class AccountType(str, Enum):
@@ -15,6 +16,9 @@ class AccountType(str, Enum):
 class UserNotFoundError(Exception):
     pass
 
+def is_all_digits(value: str) -> bool:
+    return bool(re.match(r'^\d+$', value))
+
 # Pydantic model for user
 class UserModel(BaseModel):
     name: str = Field(..., min_length=1, max_length=100)
@@ -23,6 +27,12 @@ class UserModel(BaseModel):
     address: str = Field(..., min_length=1, max_length=255)
     mobileno: str = Field(..., min_length=10, max_length=15)
     account_type: AccountType = AccountType.FREE
+
+    @field_validator('mobileno')
+    def validate_mobileno(cls, value: str) -> str:
+        if not is_all_digits(value):
+            raise ValueError("Mobile number must contain only digits")
+        return value
 
 # Dataclass for user
 @dataclass
@@ -36,3 +46,23 @@ class User:
 
 # List to store users
 users: List[User] = []
+
+
+# Function to add a user
+def add_user(user_data: dict):
+    try:
+        user = User(**user_data)
+        users.append(user)
+    except ValidationError as e:
+        raise UserNotFoundError(e)
+    
+
+# Serialize users to JSON
+def serialize_users() -> str:
+    return json.dumps([user.model_dump() for user in users], indent=4)
+
+# Deserialize users from JSON
+def deserialize_users(json_data: str):
+    user_list = json.loads(json_data)
+    for user_data in user_list:
+        add_user(user_data)
